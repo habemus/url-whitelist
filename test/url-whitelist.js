0062a31b426213e02fceea999edadac298b41dee
Simon <simonxd@gmail.com>
2016-08-14 01:05:52 -0300

    initial

---
+++ test/url-whitelist.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const should = require('should');
+
+const urlWhitelist = require('../lib');
+
+describe('url-whitelist', function () {
+
+  it('should return true for urls exactly defined in the whitelist', function () {
+
+    const isWhitelisted = urlWhitelist([
+      'http://localhost:4000',
+      'http://some.domain.com',
+    ]);
+
+    isWhitelisted('http://localhost:4000').should.equal(true);
+    isWhitelisted('http://some.domain.com').should.equal(true);
+  });
+
+  it('should return false for urls different from the whitelist', function () {
+
+    const isWhitelisted = urlWhitelist([
+      'http://localhost:4000',
+      'http://some.domain.com',
+    ]);
+
+    isWhitelisted('http://localhost:3000').should.equal(false);
+    isWhitelisted('http://some.other.domain.com').should.equal(false);
+  });
+
+  it('should return true for urls that match a pattern in the whitelist', function () {
+
+    const isWhitelisted = urlWhitelist([
+      'http://localhost:4000',
+      'http://*.domain.com',
+    ]);
+
+    isWhitelisted('http://localhost:4000').should.equal(true);
+    isWhitelisted('http://some.domain.com').should.equal(true);
+    isWhitelisted('http://another.domain.com').should.equal(true);
+    isWhitelisted('http://yet.another.domain.com').should.equal(true);
+  });
+
+  it('should accept a whitespace separated list of urls in the form of a string', function () {
+    const isWhitelisted = urlWhitelist(
+      'http://localhost:4000 https://*.domain.com   http://another.com'
+    );
+
+    isWhitelisted('http://localhost:4000').should.equal(true);
+    isWhitelisted('https://some.domain.com').should.equal(true);
+    isWhitelisted('https://another.domain.com').should.equal(true);
+    isWhitelisted('https://yet.another.domain.com').should.equal(true);
+    isWhitelisted('http://another.com').should.equal(true);
+
+    isWhitelisted('http://yet.another.com').should.equal(false);
+    isWhitelisted('http://localhost:3000').should.equal(false);
+  });
+
+});
